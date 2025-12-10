8c4d0906ba268efef32df1a6eb09fea08a5634b0
Maximiliano Aguirre Alvarez <yuka.92@hotmail.com>
2025-12-10 20:41:13 -0300

    contacto modificacion

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("JS funcionando...");
+
+const form = document.querySelector("form");
+
+if (form) {
+    form.addEventListener("submit", (e) => {
+        const email = document.querySelector("#email").value;
+        const mensaje = document.querySelector("#mensaje").value;
+
+        if (email === "" || mensaje === "") {
+            e.preventDefault();
+            alert("Por favor completá todos los campos.");
+            return;
+        }
+
+        alert("Mensaje enviado correctamente!");
+    });
+}
